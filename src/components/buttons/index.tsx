4e2589f37bd422fbacb5e9a1faae744ff44d3d78
tehtrippy <parinya.rueangphun@gmail.com>
2023-01-06 15:52:52 +0700

    style: change colors in home page

--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -1,4 +1,3 @@
-export { default as ReadMoreButton } from '@/components/buttons/ReadMoreButton';
 export { default as ShareButton } from '@/components/buttons/ShareButton';
 export { default as PrimaryButton } from '@/components/buttons/PrimaryButton';
 export { default as OutlineButton } from '@/components/buttons/OutlineButton';
