8326a7ca4e4d4d0653f5ce7f8924079a97459b81
tehtrippy <parinya.rueangphun@gmail.com>
2023-01-06 16:30:55 +0700

    style: change colors in about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,8 +5,9 @@ import { Link, useI18next, useTranslation } from 'gatsby-plugin-react-i18next';
 import { graphql, PageProps } from 'gatsby';
 import { PrimaryButton } from '@/components/buttons';
 import { MissionCard } from '@/components/cards';
-import { StaticImage } from 'gatsby-plugin-image';
 import { useMd } from '@/hooks/responsive';
+import Logo from '@/images/logo.inline.svg';
+import Career from '@images/career.inline.svg';
 
 type AboutPageProps = PageProps<GatsbyTypes.AboutPageQuery>;
 
@@ -21,26 +22,25 @@ const AboutPage: React.FC<AboutPageProps> = ({ data }) => {
   return (
     <MainLayout>
       {/* Section 1: Tong Hua Asset Management */}
-      <section className="full-page bg-gradient-to-t from-sky-400 via-transparent to-transparent lg:bg-gradient-to-l">
-        <div className="h-full flex flex-col justify-center lg:flex-row lg:justify-between items-center max-w-7xl mx-auto">
-          <div className="h-1/2 lg:h-full px-4 md:px-6 lg:px-16 xl:px-28 2xl:px-0 w-full lg:w-1/2 flex flex-col justify-center space-y-10">
-            <h1 className="font-bold text-primary-main text-4xl md:text-5xl lg:text-6xl whitespace-pre-line md:whitespace-normal lg:whitespace-pre-line">
-              {t('pages.about.section-1.header-1')}
-            </h1>
-            <div className="bg-primary-main w-16 h-0.5" />
-          </div>
-          <div className="h-1/2 w-full lg:w-1/2 lg:h-full flex flex-col justify-center items-center">
-            <StaticImage
-              src="../images/icon-512x512.png"
-              alt="thamc"
-              className="w-64 h-64 md:w-80 md:h-80"
+      <section className="full-page bg-gradient-to-t from-primary-focus via-transparent to-transparent lg:bg-gradient-to-l">
+        <div className="h-full flex flex-col justify-center lg:flex-row lg:justify-between items-center px-4 md:px-6 lg:px-16 xl:px-28 2xl:px-0 max-w-7xl mx-auto">
+          <div className="h-1/2 lg:h-full w-full lg:w-1/2 flex flex-col justify-center space-y-10">
+            <UnderlineHeader
+              title={t('pages.about.section-1.header-1')}
+              textClassName="font-bold text-4xl md:text-5xl lg:text-6xl whitespace-pre-line md:whitespace-normal lg:whitespace-pre-line text-primary-main"
+              heading="h1"
+              className="w-full !items-start"
+              underlineClassName="!mt-10"
             />
+          </div>
+          <div className="h-1/2 w-full lg:w-1/2 lg:h-full flex flex-col justify-center items-center lg:items-end">
+            <Logo className="w-full md:w-auto" />
           </div>
         </div>
       </section>
 
       {/* Section 2: ดำเนินธุรกิจด้วยความซื่อสัตย์และมั่นคงพร้อมก้าวสู่ยุคใหม่เพื่อขยายธุรกิจให้เติบโตอย่างยั่งยืน */}
-      <section className="bg-slate-600">
+      <section className="bg-neutral-100">
         <div className="px-4 py-28 lg:py-32 md:px-6 lg:px-16 xl:px-28 2xl:px-0 max-w-7xl mx-auto">
           <h2 className="text-3xl font-bold text-primary-main text-center md:whitespace-pre-line">
             {t(
@@ -53,11 +53,11 @@ const AboutPage: React.FC<AboutPageProps> = ({ data }) => {
       </section>
 
       {/* Section 3: เกี่ยวกับเรา */}
-      <section className="px-4 py-28 lg:py-32 md:px-6 lg:px-16 xl:px-28 2xl:px-0 max-w-7xl mx-auto space-y-16 flex flex-col items-center lg:flex-row">
-        <div className="flex flex-col items-start space-y-6 lg:w-3/4">
+      <section className="px-4 py-28 lg:py-32 md:px-6 lg:px-16 xl:px-28 2xl:px-0 max-w-7xl mx-auto space-y-16 lg:space-y-0 flex flex-col items-center lg:flex-row">
+        <div className="flex flex-col items-start space-y-6 lg:w-3/4 lg:pr-10">
           <UnderlineHeader
             title={t('pages.about.section-3.header-1')}
-            textClassName="text-4xl"
+            textClassName="text-4xl text-neutral-900"
             className="!items-start"
             heading="h2"
           />
@@ -69,18 +69,14 @@ const AboutPage: React.FC<AboutPageProps> = ({ data }) => {
           />
           <p>{t('pages.about.section-3.desc-2')}</p>
         </div>
-        <StaticImage
-          src="../images/icon-512x512.png"
-          alt="thamc"
-          className="w-72 h-72 lg:w-1/4"
-        />
+        <Logo className="w-72 lg:w-1/4" />
       </section>
 
       {/* Section 4: พันธกิจ */}
       <section className="px-4 pb-28 lg:pb-32 md:px-6 lg:px-16 xl:px-28 2xl:px-0 max-w-7xl mx-auto flex flex-col items-center">
         <UnderlineHeader
           title={t('pages.home.section-2.header-1')}
-          textClassName="text-4xl"
+          textClassName="text-4xl text-neutral-900"
           heading="h2"
         />
         <p className="text-lg text-center mt-6">
@@ -105,11 +101,7 @@ const AboutPage: React.FC<AboutPageProps> = ({ data }) => {
 
       {/* Section 5: สนใจร่วมงานกับเรา */}
       <section className="border border-primary-main rounded-2xl mb-28 px-4 py-10 mx-4 md:mx-6 lg:mx-16 xl:mx-28 2xl:px-0 max-w-7xl 2xl:mx-auto flex flex-col items-center justify-center lg:flex-row lg:justify-around">
-        <StaticImage
-          src="../images/icon-512x512.png"
-          alt="thamc"
-          className="w-80"
-        />
+        <Career className="w-80" />
         <div className="mt-16 flex flex-col items-center lg:items-start lg:mt-0">
           <h2 className="text-4xl font-bold text-primary-main">
             {t('pages.about.section-4.header-1')}
