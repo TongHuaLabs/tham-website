ff0d3d89ce334153dabfb0c22a6d78ff4da08f8d
tehtrippy <parinya.rueangphun@gmail.com>
2023-01-06 16:49:42 +0700

    style: change colors in 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ const NotFoundPage: React.FC<{}> = () => {
   const { t } = useTranslation();
   return (
     <MainLayout>
-      <div className="landing items-center flex justify-center lg:justify-between flex-col lg:flex-row px-4 py-10 md:px-24 lg:px-20 max-w-7xl mx-auto">
+      <div className="full-page items-center flex justify-center lg:justify-between flex-col lg:flex-row px-4 py-10 md:px-24 lg:px-20 max-w-7xl mx-auto">
         <StaticImage
           src="../images/404-unplugged.svg"
           alt="404 NotFound"
@@ -23,19 +23,19 @@ const NotFoundPage: React.FC<{}> = () => {
           />
           <div className="flex flex-col">
             <h1 className="text-neutral-900 font-bold text-xl md:text-3xl">
-              {t('Pages.NotFound.Title')}
+              {t('pages.notfound.section-1.header-1')}
             </h1>
             <p className="border-b-2 border-primary-main border w-12" />
           </div>
           <div className="flex items-center justify-between w-full">
             <Link to="/" className="w-[48.5%]">
               <PrimaryButton
-                title={t('Pages.NotFound.BackToHomeButton')}
-                className="w-full"
+                title={t('components.buttons.go-to-home')}
+                className="w-full justify-center"
               />
             </Link>
             <OutlineButton
-              title={t('Pages.NotFound.GoBackButton')}
+              title={t('components.buttons.go-back')}
               className="w-[48.5%] border-primary-main text-primary-main font-bold hover:bg-primary-main hover:text-neutral-50 delay-100"
               onClick={() => navigate(-1)}
             />
