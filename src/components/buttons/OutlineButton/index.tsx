d346b0f4ec81d47ce770af8103d4487406794638
tehtrippy <parinya.rueangphun@gmail.com>
2023-01-20 16:12:56 +0700

    style: notfound page

--- src/components/buttons/OutlineButton/index.tsx
+++ src/components/buttons/OutlineButton/index.tsx
@@ -15,7 +15,7 @@ const OutlineButton: React.FC<OutlineButtonProps> = ({
   return (
     <button
       onClick={onClick}
-      className={classNames('border py-1.5 rounded-lg px-4', className)}
+      className={classNames('border py-1.5 rounded px-4', className)}
     >
       {title}
     </button>
