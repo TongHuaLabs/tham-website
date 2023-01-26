8b24ba4eb30b81d4718acbe529b171850705529e
tehtrippy <parinya.rueangphun@gmail.com>
2023-01-26 17:03:16 +0700

    style: notfound page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,39 +9,34 @@ const NotFoundPage: React.FC<{}> = () => {
   const { t } = useTranslation();
   return (
     <MainLayout>
-      <div className="full-page items-center flex justify-center lg:justify-between flex-col lg:flex-row px-4 py-10 md:px-24 lg:px-20 max-w-7xl mx-auto">
-        <StaticImage
-          src="../images/404-unplugged.svg"
-          alt="404 NotFound"
-          className="self-end lg:self-center"
-        />
-        <div className="flex flex-col space-y-10 lg:w-[45%]">
-          <StaticImage
-            src="../images/404-state.svg"
-            alt="404 NotFound"
-            className="w-1/2 lg:w-full lg:self-center"
-          />
-          <div className="flex flex-col">
-            <h1 className="text-neutral-900 font-bold text-xl md:text-3xl">
-              {t('pages.notfound.section-1.header-1')}
+      <section className="full-page bg-gradient-to-b from-primary-focus/10 to-transparent">
+        <div className="h-full flex flex-col justify-center space-y-16 items-center px-4 py-28 lg:py-32 md:px-6 lg:px-16 xl:px-28 2xl:px-0 max-w-7xl mx-auto">
+          <div className="flex flex-col space-y-4 items-center">
+            <StaticImage src="../images/404.png" alt="404 NotFound" />
+            <h1 className="text-2xl md:text-4xl font-medium text-primary-main">
+              Page not found
             </h1>
-            <p className="border-b-2 border-primary-main border w-12" />
           </div>
-          <div className="flex items-center justify-between w-full">
-            <Link to="/" className="w-[48.5%]">
-              <PrimaryButton
-                title={t('components.buttons.go-to-home')}
-                className="w-full justify-center"
+          <div className="flex flex-col space-y-10 w-full">
+            <h2 className="text-neutral-900 font-bold text-2xl md:text-4xl text-center whitespace-pre-wrap md:whitespace-normal">
+              {t('pages.notfound.section-1.header-1')}
+            </h2>
+            <div className="flex items-center justify-between w-full 2xl:w-2/3 2xl:mx-auto">
+              <Link to="/" className="w-[48.5%]">
+                <PrimaryButton
+                  title={t('components.buttons.go-to-home')}
+                  className="w-full justify-center"
+                />
+              </Link>
+              <OutlineButton
+                title={t('components.buttons.go-back')}
+                className="w-[48.5%] border-primary-main text-primary-main font-bold hover:bg-primary-main hover:text-neutral-50 delay-100"
+                onClick={() => navigate(-1)}
               />
-            </Link>
-            <OutlineButton
-              title={t('components.buttons.go-back')}
-              className="w-[48.5%] border-primary-main text-primary-main font-bold hover:bg-primary-main hover:text-neutral-50 delay-100"
-              onClick={() => navigate(-1)}
-            />
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </MainLayout>
   );
 };
